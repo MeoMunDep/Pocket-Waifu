cd681a6cb6f59448cd66d9a1800e7172896cd21a
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-05-28 02:35:46 +0700

    fix .sh (2)

--- setup.sh
+++ setup.sh
@@ -87,7 +87,7 @@ while true; do
 
             check_configs
 
-            for file in datas.txt wallets.txt proxies.txt; do
+            for file in datas.txt proxies.txt; do
                 if [ ! -f "$file" ]; then
                     touch "$file"
                     print_green "Created $file"
